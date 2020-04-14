bd09d357984eb44593fdca3a40ef5b284856b7bb
rgh1002 <hopk8412@gmail.com>
2020-04-13 20:40:06 -0400

    Added ability to get user input from GUI, added separate class files for GUI and event listener, added event listener for action on button click

---
+++ src/edu/SRUJFuzzyProj/Listener.java
@@ -0,0 +1,14 @@
+package edu.SRUJFuzzyProj;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Listener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// What should happen when button is pressed:
+		System.out.println("Button was pressed!");
+	}
+	
+}
